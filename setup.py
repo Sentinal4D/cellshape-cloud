a94010bf5fd861189aeccd588fd81482e74049a0
DeVriesMatt <matthew.devries.13@gmail.com>
2022-09-05 10:37:07 +0100

    Bump version: 0.0.26-rc0 → 0.0.27-rc0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name="cellshape-cloud",
-    version="0.0.26-rc0",
+    version="0.0.27-rc0",
     description="3D cell shape analysis using geometric deep"
     " learning on point clouds",
     long_description=long_description,
