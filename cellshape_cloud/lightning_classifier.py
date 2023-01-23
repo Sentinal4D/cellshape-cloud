d7fe0917c935b7221d7e83a56bb12eaf7e947b10
DeVriesMatt <matthew.devries.13@gmail.com>
2023-01-23 21:22:30 +0000

    update for supervised

--- cellshape_cloud/lightning_classifier.py
+++ cellshape_cloud/lightning_classifier.py
@@ -15,15 +15,72 @@ class CloudClassifierPL(pl.LightningModule):
         self.encoder_type = args.encoder_type
         self.model = model
 
-        def configure_optimizers(self):
-            optimizer = torch.optim.Adam(
-                self.parameters(),
-                lr=self.lr,
-                betas=(0.9, 0.999),
-                weight_decay=1e-6,
-            )
-            return optimizer
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.lr,
+            betas=(0.9, 0.999),
+            weight_decay=1e-6,
+        )
+        return optimizer
 
-        def encode(self, x):
-            z = self.model(x)
-            return z
+    def encode(self, x):
+        z = self.model(x)
+        return z
+
+    def load_model(self, path):
+        checkpoint = torch.load(path, map_location="cuda:0")
+        model_dict = (
+            self.model.state_dict()
+        )  # load parameters from pre-trained FoldingNet
+
+        for k in checkpoint["model_state_dict"]:
+            if k in model_dict:
+                model_dict[k] = checkpoint["model_state_dict"][k]
+                print("    Found weight: " + k)
+            elif k.replace("folding1", "folding") in model_dict:
+                model_dict[k.replace("folding1", "folding")] = checkpoint[
+                    "model_state_dict"
+                ][k]
+                print("    Found weight: " + k)
+        print("Done loading encoder")
+
+        self.model.load_state_dict(model_dict)
+
+    def load_lightning(self, path):
+        checkpoint = torch.load(
+            path, map_location=lambda storage, loc: storage
+        )
+        self.load_state_dict(checkpoint["state_dict"])
+
+    def load_shapenet(self, path):
+        checkpoint = torch.load(
+            path, map_location=lambda storage, loc: storage
+        )
+        # "load encoder"
+        model_dict = self.model.state_dict()
+        for k in checkpoint:
+            if k in model_dict:
+                model_dict[k] = checkpoint[k]
+                print("    Found weight: " + k)
+            elif k.replace("encoder.", "model.encoder.") in model_dict:
+                model_dict[
+                    k.replace("encoder.", "model.encoder.")
+                ] = checkpoint[k]
+                print("    Found weight: " + k)
+            elif k.replace("decoder.", "model.decoder.") in model_dict:
+                model_dict[
+                    k.replace("decoder.", "model.decoder.")
+                ] = checkpoint[k]
+                print("    Found weight: " + k)
+
+        self.model.load_state_dict(model_dict)
+
+    def training_step(self, batch, batch_idx):
+        inputs, labels = batch[2]
+        outputs, features = self.model(inputs)
+
+        loss = self.criterion(labels, outputs)
+        self.log("loss", loss)
+
+        return loss
