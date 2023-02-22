4398ccb5880b6663c8be30cc90bb6f968c2ee7af
Matt De Vries <matthew.devries.13@gmail.com>
2023-02-22 13:33:33 +0000

    Bump version: 0.1.0-rc0 → 0.1.0

--- cellshape_cloud/__init__.py
+++ cellshape_cloud/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0-rc0"
+__version__ = "0.1.0"
 from .cloud_autoencoder import CloudAutoEncoder
 from .pointcloud_dataset import PointCloudDataset
 from .training_functions import train
