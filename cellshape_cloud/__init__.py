937e22fe169ce203244da1d7da380383a8d5bdaa
DeVriesMatt <matthew.devries.13@gmail.com>
2023-04-27 12:06:25 +0100

    update init

--- cellshape_cloud/__init__.py
+++ cellshape_cloud/__init__.py
@@ -2,10 +2,12 @@ __version__ = "0.1.1"
 from .cloud_autoencoder import CloudAutoEncoder
 from .pointcloud_dataset import PointCloudDataset
 from .training_functions import train
+from .train_autoencoder import train_autoencoder
 from .reports import *
 
 __all__ = (
     "CloudAutoEncoder",
     "PointCloudDataset",
     "train",
+    "train_autoencoder",
 )
